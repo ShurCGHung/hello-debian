ba50db5f6c3b48837e86925c85c2438f174f0f47
Shurie <caogiahung2501@gmail.com>
2023-03-15 17:01:18 +0700

    update 4

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ void main(int argc, char** argv) {
   printf("hello CMake\n");
   printf("Hello github\n");
   printf("Hello Email\n");
+  printf("Hello Makefile\n");
   printf("Hello Jenkins");
   getchar();
 }
